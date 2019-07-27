16ce14fe1faf099d6e11d712a1998f28051b50b9
Domrev Igor <domrevigor@gmail.com>
2019-07-27 22:27:05 +0300

    adding jenkins dsl to idea

--- src/jenkins.groovy
+++ src/jenkins.groovy
@@ -1,8 +1,3 @@
-//https://gist.github.com/arehmandev/736daba40a3e1ef1fbe939c6674d7da8
-// getting jenkins dsl to work with idea
-
-//https://st-g.de/2016/08/jenkins-pipeline-autocompletion-in-intellij
-//http://localhost:8080/job/go_alfred/pipeline-syntax/gdsl << GET THE DSL
 pipeline {
     agent any()
     environment {

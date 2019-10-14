142b15934572cf3805ba4d30e9cf5c041e00a5ed
Domrev Igor <domrevigor@gmail.com>
2019-10-14 14:08:48 +0300

    an alfred workflow that lists all open pull requests

--- github-pull-requests/deploy.sh
+++ github-pull-requests/deploy.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-go build -o PullRequests main.go
-aws s3 --profile personal_s3 cp PullRequests  s3://opentikva/ --acl public-read
-mv PullRequests /Users/domrevigor/Dropbox/alfred/Alfred.alfredpreferences/workflows/user.workflow.7B1E72CE-CBC3-4366-B2A1-0D91E1C573ED/
+go build -o Pullrequests main.go
+aws s3 --profile personal_s3 cp Pullrequests  s3://opentikva/ --acl public-read
+mv Pullrequests /Users/domrevigor/Dropbox/alfred/Alfred.alfredpreferences/workflows/user.workflow.7B1E72CE-CBC3-4366-B2A1-0D91E1C573ED/
